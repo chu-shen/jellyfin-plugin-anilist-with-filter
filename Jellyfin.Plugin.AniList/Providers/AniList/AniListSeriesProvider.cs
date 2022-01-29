49a9e66bf3216da56f3814ed4c99e499d4d2e875
初神 <45256288+chu-shen@users.noreply.github.com>
2022-01-29 23:50:54 +0800

    Update AniListSeriesProvider.cs

--- Jellyfin.Plugin.AniList/Providers/AniList/AniListSeriesProvider.cs
+++ Jellyfin.Plugin.AniList/Providers/AniList/AniListSeriesProvider.cs
@@ -48,7 +48,7 @@ namespace Jellyfin.Plugin.AniList.Providers.AniList
             {
                 _log.LogInformation("Start AniList ... before Searching ({Name})", info.Name);   
                 
-                BasicFilter basicFilter = new BasicFilter(_log);
+                BasicFilter basicFilter = new BasicFilter();
                 string searchName = basicFilter.GetRealName(info.Name);
                 
                 _log.LogInformation("Start AniList ... Searching the correct anime({Name})", searchName);  
