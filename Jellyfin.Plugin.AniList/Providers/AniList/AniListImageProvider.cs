44e0649e6a81de68420f8f4cade165b52089e11e
初神 <45256288+chu-shen@users.noreply.github.com>
2022-01-30 05:26:04 +0800

    Update AniListImageProvider.cs

--- Jellyfin.Plugin.AniList/Providers/AniList/AniListImageProvider.cs
+++ Jellyfin.Plugin.AniList/Providers/AniList/AniListImageProvider.cs
@@ -74,6 +74,7 @@ namespace Jellyfin.Plugin.AniList.Providers.AniList
 
         public async Task<HttpResponseMessage> GetImageResponse(string url, CancellationToken cancellationToken)
         {
+            await AniListSeriesProvider.RequestLimiter.Tick().ConfigureAwait(false);
             var httpClient = Plugin.Instance.GetHttpClient();
             return await httpClient.GetAsync(url).ConfigureAwait(false);
         }
