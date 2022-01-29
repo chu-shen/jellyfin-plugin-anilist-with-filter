3d20a6fd67ac3fc8bc3b42c43b604857820023a9
初神 <45256288+chu-shen@users.noreply.github.com>
2022-01-29 23:29:38 +0800

    添加截取部分文件名 | Add intercepted part file name

--- Jellyfin.Plugin.AniList/Filter/BasicFilter.cs
+++ Jellyfin.Plugin.AniList/Filter/BasicFilter.cs
@@ -28,7 +28,6 @@ namespace Jellyfin.Plugin.AniList.Filter
         /// <returns></returns>
         public string GetRealName(string searchName)
         {    
-            _log.LogInformation("Start AniList ... before Searching ({Name})", searchName);   
 
             // 去除集名
             // quick remove info
@@ -74,21 +73,48 @@ namespace Jellyfin.Plugin.AniList.Filter
             // 去除空格
             // Remove whitespace
             searchName = searchName.Trim();
-   
-            _log.LogInformation("Start AniList ... Searching the correct anime({Name})", searchName);  
 
+            return searchName;
+        }
+        
+        
+        /// <summary>
+        ///获取部分文件名
+        ///get part of file name
+        ///如果搜索结果为空，不直接返回，尝试改变标题重新搜索
+        ///do not return directly, try to change the title and search again
+        ///TODO a better strategy
+        /// </summary>
+        /// <param name="searchName">文件名 searchName</param>
+        /// <returns></returns>
+        public string GetPartName(string searchName)
+        {   
+            // 通常，动画是罗马音或原日文标题，比较规范。而里番大概率是罗马音与日文混用或标题名与集名混用，这种情况基本搜索失败
+            // 因此判断为非仅字母数字组成的文件名搜索失败时，尝试只使用部分文件名进行搜索，以空格分割
+            // anime(title)->romaji/japanese; R18 anime(title + episode)->japanese + romaji/english
+            string numAndLetter = @"^[A-Za-z0-9]+$";
+            Regex numAndLetterRegex = new Regex(numAndLetter);                
+            string onlyLetter = @"^[a-zA-Z]+$";
+            Regex onlyLetterRegex = new Regex(onlyLetter);
+            if (!numAndLetterRegex.IsMatch(searchName) && !onlyLetterRegex.IsMatch(searchName))
+            {
+                // return string before first space
+                searchName = searchName.Split(' ')[0];
+            }
+            
             return searchName;
         }
 
         /// <summary>
         ///根据关键词，去除关键词及之后所有非必要部分，如：集名
         ///quick remove unnecesary info, eg:epside title
+        ///TODO filter regex
         /// </summary>
         /// <param name="searchName">文件名 searchName</param>
         /// <returns></returns>
         private string quickRemoveInfo(string searchName)
         {
-            string[] quickRemoveEP = {"vol", "下巻", "上巻", "EPISODE", "第1話", "第一話", "第一章", "第一话", "#","上卷", "下卷"};
+            string[] quickRemoveEP = {"vol", "#", "下巻", "上巻","上卷", "下卷", "EPISODE", "第1話", "第2話", "第一話", "第二話", "第一章", "第二章", "第一话", "第二话"};
             
             foreach(string c in quickRemoveEP)
                 Regex r = new Regex(c);
