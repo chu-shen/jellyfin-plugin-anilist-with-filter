8753dee6cb934bc825d46fa1c9691f55d85b095c
初神 <45256288+chu-shen@users.noreply.github.com>
2022-01-30 16:00:56 +0800

    remove file extension

--- Jellyfin.Plugin.AniList/Providers/AniList/AniListSeriesProvider.cs
+++ Jellyfin.Plugin.AniList/Providers/AniList/AniListSeriesProvider.cs
@@ -29,11 +29,11 @@ namespace Jellyfin.Plugin.AniList.Providers.AniList
         
         // NOTE: AniDB has very low request rate limits, a minimum of 2 seconds between requests, and an average of 4 seconds between requests
         // NOTE: anilist 90 requests per minute, more info -> https://anilist.gitbook.io/anilist-apiv2-docs/overview/rate-limiting
-        // formula：speed = series Task + movie Task + image Task < 90 Task/per minute  eg: 1min/2sec = 30 < 90
-        // 每分钟的请求总数为30，每个请求间的间隔时间必须在1s以上。具体请求时间自行记录观察当前三种请求的时间点
-        // The total number of requests per minute is 30, and the interval between requests must be more than 1s
+        // formula：speed = series Task + movie Task + image Task < 90 Task/per minute  eg: 1min/1sec = 60 < 90
+        // 每分钟的请求总数为60，每个请求间的间隔时间必须在500ms以上。具体请求时间自行记录观察当前三种请求的时间点
+        // The total number of requests per minute is 60, and the interval between requests must be more than 500ms
         // 三种请求均使用AniListSeriesProvider中同一个RequestLimiter
-        public static readonly RateLimiter RequestLimiter = new RateLimiter(TimeSpan.FromSeconds(1), TimeSpan.FromSeconds(2), TimeSpan.FromMinutes(1));
+        public static readonly RateLimiter RequestLimiter = new RateLimiter(TimeSpan.FromMilliseconds(500), TimeSpan.FromSeconds(1), TimeSpan.FromMinutes(1));
 
 
         public AniListSeriesProvider(IApplicationPaths appPaths, ILogger<AniListSeriesProvider> logger)
@@ -58,8 +58,8 @@ namespace Jellyfin.Plugin.AniList.Providers.AniList
             {
                 //https://github.com/jellyfin/jellyfin/blob/master/Emby.Naming/TV/SeriesInfo.cs
                 //https://github.com/jellyfin/jellyfin/blob/f863ca1f2d00839fd78a7655c759e25e9815483f/Emby.Naming/TV/SeriesResolver.cs#L43
-                // use true file name ,not info.Name. because it will change to something else 
-                string searchName = Path.GetFileName(info.Path);
+                // use true file name(without extension) ,not info.Name. because it will change to something else 
+                string searchName = Path.GetFileNameWithoutExtension(info.Path);
                 _log.LogDebug("Start AniList ... before Searching ({Name})", searchName); 
                 
                 BasicFilter basicFilter = new BasicFilter(_log);
