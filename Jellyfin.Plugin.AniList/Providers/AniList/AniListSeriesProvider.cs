2d44ab122a85764c5c486c69001f1cd452f0a03d
初神 <45256288+chu-shen@users.noreply.github.com>
2022-02-03 21:37:32 +0800

    Update AniListSeriesProvider.cs

--- Jellyfin.Plugin.AniList/Providers/AniList/AniListSeriesProvider.cs
+++ Jellyfin.Plugin.AniList/Providers/AniList/AniListSeriesProvider.cs
@@ -60,7 +60,7 @@ namespace Jellyfin.Plugin.AniList.Providers.AniList
                 _log.LogInformation("Start AniList ... Searching the correct anime({Name})", searchName);  
 
                 _log.LogTrace(System.DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss:fff")+":series requet time");
-                await RequestLimiter.Tick().ConfigureAwait(false);
+                await AniListResultProvider.RequestLimiter.Tick().ConfigureAwait(false);
                 await Task.Delay(Plugin.Instance.Configuration.AniDbRateLimit).ConfigureAwait(false);
 
                 MediaSearchResult msr = await _aniListApi.Search_GetSeries(searchName, cancellationToken);
